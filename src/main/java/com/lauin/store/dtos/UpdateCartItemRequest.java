4ef946a45bd122b8a2d4fe15707ce35ef4f62e6d
Lauin Hamow <lauin.hamow@rub.de>
2025-08-19 12:41:19 +0200

    adding the shopping cart logic

---
+++ src/main/java/com/lauin/store/dtos/UpdateCartItemRequest.java
@@ -0,0 +1,14 @@
+package com.lauin.store.dtos;
+
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
+public class UpdateCartItemRequest {
+    @NotNull(message = "Quantity must be provided.")
+    @Min(value = 1, message = "Quantity must be greater than zero.")
+    @Max(value = 1000, message = "Quantity must be equal or less than 1000")
+    private Integer quantity;
+}
---
+++ src/main/java/com/lauin/store/exceptions/CartNotFoundException.java
@@ -0,0 +1,4 @@
+package com.lauin.store.exceptions;
+
+public class CartNotFoundException extends RuntimeException {
+}
